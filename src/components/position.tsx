ee267ebf0b9f58419b70bdb656226e59e0c4d335
byLuisfer <luisferdass@gmail.com>
2025-01-20 09:53:18 +0100

    Add React and add text animation

---
+++ src/components/position.tsx
@@ -0,0 +1,27 @@
+export const TextRevealTW = () => {
+  const text = [
+    "Applikationsentwickler", 
+    "in der Schweiz"
+  ]; // Define el texto como un array de líneas
+
+  return (
+    <h1 className="overflow-hidden text-7xl font-InstrumentSerif text-white leading-tight mt-5 cursor-default">
+      {text.map((line, lineIndex) => (
+        <div key={lineIndex} className="block">
+          {line.split("").map((char, charIndex) => (
+            <span
+              className="animate-text-reveal inline-block [animation-fill-mode:backwards]"
+              key={`${char}-${lineIndex}-${charIndex}`}
+              style={{ animationDelay: `${(lineIndex * line.length + charIndex) * 0.04}s` }}
+            >
+              {char === " " ? "\u00A0" : char}
+            </span>
+          ))}
+          {lineIndex < text.length - 1 && <br />} {/* Agrega un <br> excepto en la última línea */}
+        </div>
+      ))}
+    </h1>
+  );
+};
+
+export default TextRevealTW;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,31 @@ export default {
 			fontFamily: {
 				InstrumentSerif: ['InstrumentSerif'],
 				MonaSans: ['MonaSans']
-			}
+			},
+			animation: {
+				"text-reveal": "text-reveal 1.5s cubic-bezier(0.77, 0, 0.175, 1) 0.5s forwards",
+				"text-up": "text-up 0.8s",
+			  },
+			  keyframes: {
+				"text-reveal": {
+				  "0%": {
+					transform: "translateY(100%)",
+					opacity: "0",
+				  },
+				  "100%": {
+					transform: "translateY(0)",
+					opacity: "1",
+				  },
+				},
+				"text-up": {
+				  "0%": {
+					transform: "translateY(100%)",
+				  },
+				  "100%": {
+					transform: "translateY(0)",
+				  },
+				},
+			},
 		},
 	},
 	plugins: [],
