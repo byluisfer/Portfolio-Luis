eeb2a1b5a4d7f1e8aabd340fdf5c6b9365b00ca5
byLuisfer <luisferdass@gmail.com>
2025-01-29 14:21:51 +0100

    Fixed bug on scroll when go up or down

--- public/scripts/smoothScroll.js
+++ public/scripts/smoothScroll.js
@@ -1,31 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let scrollY = window.scrollY;
-    let isScrolling = false;
-  
-    function smoothScroll(event) {
-      event.preventDefault();// Stop the default scroll
-      let delta = event.deltaY * 0.6; // Control the speed of the scroll
+  let scrollY = window.scrollY;
+  let isScrolling = false;
+
+  function smoothScroll(event) {
+      event.preventDefault(); // Stop the scroll by default
+      let delta = event.deltaY * 0.6;
       scrollY += delta;
-  
+
+      // Asegure that scrollY does not exceed the upper or lower limit
+      scrollY = Math.max(0, Math.min(scrollY, document.documentElement.scrollHeight - window.innerHeight));
+
       if (!isScrolling) {
-        isScrolling = true;
-        requestAnimationFrame(animateScroll);
+          isScrolling = true;
+          requestAnimationFrame(animateScroll);
       }
-    }
-  
-    function animateScroll() {
+  }
+
+  function animateScroll() {
       let currentScroll = window.scrollY;
       let diff = scrollY - currentScroll;
-      
-      window.scrollBy(0, diff * 0.2); // Control the smoothness of the scroll
-  
+
       if (Math.abs(diff) > 0.5) {
-        requestAnimationFrame(animateScroll);
+          window.scrollBy(0, diff * 0.2);
+          requestAnimationFrame(animateScroll);
       } else {
-        isScrolling = false;
+          isScrolling = false;
+          scrollY = window.scrollY;
       }
-    }
-  
-    document.addEventListener("wheel", smoothScroll, { passive: false }); // Scroll with mouse wheel
-    document.addEventListener("touchmove", smoothScroll, { passive: false }); // Scroll with touchpad
-  });
+  }
+
+  document.addEventListener("wheel", smoothScroll, { passive: false });
+  document.addEventListener("touchmove", smoothScroll, { passive: false });
+});
