ee8db7f9c4cf6aa0e67a2d3c402da64ad5a31f48
byLuisfer <luisferdass@gmail.com>
2025-01-29 14:24:10 +0100

    Bug fixed: Now the scroll on phones work nice

--- public/scripts/smoothScroll.js
+++ public/scripts/smoothScroll.js
@@ -1,13 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   let scrollY = window.scrollY;
   let isScrolling = false;
+  let touchStartY = 0;
 
   function smoothScroll(event) {
-      event.preventDefault(); // Stop the scroll by default
+      event.preventDefault(); // Stop the default scroll
       let delta = event.deltaY * 0.6;
+      updateScroll(delta);
+  }
+
+  function touchStart(event) {
+      touchStartY = event.touches[0].clientY;
+  }
+
+  function touchMove(event) {
+      event.preventDefault();
+      let touchY = event.touches[0].clientY;
+      let delta = (touchStartY - touchY) * 0.6;
+      updateScroll(delta);
+      touchStartY = touchY;
+  }
+
+  function updateScroll(delta) {
       scrollY += delta;
 
-      // Asegure that scrollY does not exceed the upper or lower limit
+      // Make sure that scrollY does not exceed the upper or lower limit
       scrollY = Math.max(0, Math.min(scrollY, document.documentElement.scrollHeight - window.innerHeight));
 
       if (!isScrolling) {
@@ -30,5 +47,6 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   document.addEventListener("wheel", smoothScroll, { passive: false });
-  document.addEventListener("touchmove", smoothScroll, { passive: false });
+  document.addEventListener("touchstart", touchStart, { passive: false });
+  document.addEventListener("touchmove", touchMove, { passive: false });
 });
